56a9662ff9ce281bfa6bbac29c630fd781972b36
Scott Berry <berryscottr@gmail.com>
2021-06-24 19:10:46 -0400

    side deck selection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut side_deck = String::new();
+    println!("Enter your side deck in a space separated list\nInvalid entries will result in four random cards");
+    io::stdin()
+        .read_line(&mut side_deck)
+        .ok()
+        .expect("No side deck entered");
+    let side_deck: Vec<u32> = side_deck
+        .split_whitespace()
+        .map(|s| s.parse().expect("parse error"))
+        .collect();
+    println!("Your side deck is: ");
+    for card in side_deck {
+        println!("{}", card)
+    }
+    println!("Let's play Pazaak!");
+    // let mut player_a_score = 0;
+    // let mut player_b_score = 0;
+    // loop {
+    //     if player_a_score = 20 || if player_b_score = 20 { }
+    // }
 }
